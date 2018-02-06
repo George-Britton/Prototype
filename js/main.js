05fbca32e059d75e39655ec15f28cc6abae74a4c
George Britton <george.britton@student.nua.ac.uk>
2018-02-06 16:30:41 +0000

    All character sprites for house and crime scene added, as well as animations for player

--- js/main.js
+++ js/main.js
@@ -1,23 +1,70 @@
 var game = new Phaser.Game(1800, 1000, Phaser.AUTO, null, { preload: preload, create: create, update: update });
-var player, door, menuButton, booksButton;
+var scene, player, grandfather, constable, body, detective, inspector, door, background, menuButton, booksButton, destination;
 
 function preload() {
+    scene = "crime scene";
+
     this.game.load.image('house_background', 'images/house_background.png');
+    this.game.load.image('crimeScene_background', 'images/crimeScene_background.png');
     this.game.load.image('dialogueBox', 'images/dialogueBox.png');
     this.game.load.image('menuButton', 'images/menuButton.png');
     this.game.load.image('booksButton', 'images/booksButton.png');
     this.game.load.spritesheet('player', 'images/player.png', 150, 150);
-
+    this.game.load.spritesheet('grandfather', 'images/grandfather.png', 225, 225);
+    this.game.load.spritesheet('constable', 'images/constable.png', 250, 250);
+    this.game.load.spritesheet('detective', 'images/detective.png', 250, 250);
+    this.game.load.spritesheet('inspector', 'images/inspector.png', 250, 250);
+    this.game.load.image('body', 'images/body.png');
 }
 
 function create() {
-    this.game.add.image(0, 0, 'house_background');
-    this.game.add.image(0, 700, 'dialogueBox');
-    menuButton = game.add.image(1725, 630, 'menuButton');
-    booksButton = game.add.image(1635, 630, 'booksButton');
-    player = game.add.sprite(1000, 450, 'player')
+    if (scene == "house") {
+        background = game.add.image(0, 0, 'house_background');
+        this.game.add.image(0, 700, 'dialogueBox');
+        menuButton = game.add.image(1725, 630, 'menuButton');
+        booksButton = game.add.image(1635, 630, 'booksButton');
+
+        grandfather = game.add.sprite(1300, 425, 'grandfather')
+        game.physics.enable(grandfather, Phaser.Physics.ARCADE);
+
+
+        player = game.add.sprite(1000, 500, 'player')
+        game.physics.enable(player, Phaser.Physics.ARCADE);
+
+    } else if (scene == "crime scene") {
+        background = game.add.image(0, 0, 'crimeScene_background');
+        this.game.add.image(0, 700, 'dialogueBox');
+        menuButton = game.add.image(1725, 630, 'menuButton');
+        booksButton = game.add.image(1635, 630, 'booksButton');
+
+        grandfather = game.add.sprite(1300, 421, 'grandfather')
+        game.physics.enable(grandfather, Phaser.Physics.ARCADE);
+
+        constable = game.add.sprite(1450, 393, 'constable');
+        game.physics.enable(constable, Phaser.Physics.ARCADE);
+
+        body = game.add.sprite(300, 585, 'body');
+
+        player = game.add.sprite(1150, 494, 'player')
+        game.physics.enable(player, Phaser.Physics.ARCADE);
+    }
+
+    grandfather.animations.add('grandfatherIdleRight', [7], 1, true);
+    grandfather.animations.add('grandfatherIdleLeft', [4], 1, true);
+    constable.animations.add('constableIdleLeft', [4], 1, true);
+    player.animations.add('playerIdleRight', [7], 1, true);
+    player.animations.add('playerIdleLeft', [4], 1, true);
+    player.animations.add('moveRight', [8, 7, 6, 7], 6, true);
+    player.animations.add('moveLeft', [5, 4, 3, 4], 6, true);
 }
 
 function update() {
-
+    if (scene == "house") {
+        player.animations.play('playerIdleRight');
+        grandfather.animations.play('grandfatherIdleLeft');
+    } else if (scene == "crime scene") {
+        player.animations.play('playerIdleLeft');
+        grandfather.animations.play('grandfatherIdleLeft');
+        constable.animations.play('constableIdleLeft');
+    }
 }
